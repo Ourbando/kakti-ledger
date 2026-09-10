10bb91a1db13c372dd45a876545b28aa48c3855c
Ourbando <ourbando@proton.me>
2026-09-10 13:14:42 +0100

    Kakti leads the tour himself: dwell per screen, taps optional, 8 lines cut

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "kakti-1789041102";
+const CACHE = "kakti-1789042482";
 const FILES = ["./", "./index.html", "./manifest.json", "./icon-180.png", "./icon-512.png"];
 self.addEventListener("install", (e) => { e.waitUntil(caches.open(CACHE).then((c) => c.addAll(FILES)).then(() => self.skipWaiting())); });
 self.addEventListener("activate", (e) => { e.waitUntil(caches.keys().then((ks) => Promise.all(ks.filter((k) => k !== CACHE).map((k) => caches.delete(k)))).then(() => self.clients.claim())); });
